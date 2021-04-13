1eb47466b6e5c9d05766c3f138c55cc03b9ee372
Prabskuyyy <82460883+Prabskuyyy@users.noreply.github.com>
2021-04-13 22:41:00 +0700

    Update fungsi help
    implementasi help

--- tubes.py
+++ tubes.py
@@ -16,4 +16,29 @@ while True:
     elif a == 'exit':
         break
 
+x = input(">>>" )
+def help() :
+    if x == "user" :
+        print(" ================== HELP ==================")
+        print(" login - untuk melakukan login ke dalam sistem")
+        print(" carirarity - untuk mencari gadget dengan rarity tertentu")
+        print(" caritahun - untuk mencari gadget berdasarkan tahun ditemukan")
+        print(" pinjam - untuk melakukan peminjaman gadget")
+        print(" kembalikan - untuk mengembalikan gadget secara seutuhnya")
+        print(" minta - untuk meminta consumable yang tersedia")
+        print(" savedata - untuk melakukan penyimpanan ke dalam file")
+        print(" help - untuk memberikann panduan penggunaan sistem")
+    elif x == "admin" :
+        print(" ================== HELP ==================")
+        print(" register - untuk melakukan registrasi user baru")
+        print(" carararity - untuk mencari gadget dengan rarity tertentu")
+        print(" caritahun - untuk mencari gadget berdasarkan tahun ditemukan")
+        print(" tambahitem - untuk melakukan penambahan item")
+        print(" hapusitem - untuk menghapus suatu item pada database")
+        print(" ubahjumlah - untuk mengubah jumlah gadget dan consumable yang terdapat didalam sistem")
+        print(" riwayatpinjam - untuk melihat riwayat peminjaman gadget")
+        print(" riwayatkembali - untuk melihat riwayat pengembalian gadget")
+        print(" riwayatambil - untuk melihat riwayat pengambilan consumable")
+        print(" savedata - untuk melakukan penyimpanan ke dalam file")
+        print(" help - untuk memberikan panduan penggunaan sistem")
 #hello world
