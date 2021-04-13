50e8d2b031f88c8fd5110b53d72ae10b403666ff
farrel-a <70206944+farrel-a@users.noreply.github.com>
2021-04-13 22:28:50 +0700

    Membuat file tubes.py
    file awal tubes.py

---
+++ tubes.py
@@ -0,0 +1,16 @@
+def login():
+    while True:
+        a = input("Username: ")
+        p = input("Password: ")
+        if a == "farrel" and p=="rel111":
+            print("Logged in !\n")
+            break
+        else:
+            print("Wrong Username/Password")
+
+while True:
+    a = input(">>> ")
+    if a == 'login':
+        login()
+    elif a == 'exit':
+        break
