426891b05d2b1eb79a69227fbaac706e61232f31
nandono206 <79740572+nandono206@users.noreply.github.com>
2021-04-17 13:44:55 +0700

    commmit ulang nando

--- tubes.py
+++ tubes.py
@@ -29,13 +29,12 @@ def csv_reader(file_name):
         f.close()
     return z
 
-def csv_writer(file_name, isi, s): #s : status, "w" to overwrite, "a" to append
-    filename = file_name  #nama file di sini
+def csv_writer(file_name, isi, s):  # s : status, "w" to overwrite, "a" to append
+    filename = file_name  # nama file di sini
     f = open(filename, s)
-    #yang akan diisi ke file.csv
+    # yang akan diisi ke file.csv
     f.writelines(isi)
     f.close()
-
 def check(n, var_name): #mencek input pengguna saat menambah item
     if var_name == 'Jumlah' or var_name == 'tahun ditemukan':
         try:
@@ -129,147 +128,6 @@ def tambahitem():
     else:
         print("\nGagal menambahkan item karena ID tidak valid")
         return[]
-
-def login():
-    while True:
-        a = input("Username: ")
-        p = input("Password: ")
-        if a == "farrel" and p=="rel111":
-            print("Logged in !\n")
-            break
-        else:
-            print("Wrong Username/Password")
-
-def riwayatpinjam():
-    filename = "gadget_borrow_history.csv"
-    arr_ra = csv_reader(filename)
-    i = len(arr_ra)-1
-    if len(arr_ra) == 1: #data kosong
-        print("Tidak ada data peminjaman gadget.")
-    else : # len(arr_ra) != 1
-        flag1 = True
-        while flag1 :
-            flag2 = True
-            for x in range(5): #descending per 5 data
-                print(f"ID Peminjaman : {arr_ra[i][0]}")
-                print(f"Nama Pengambil : {arr_ra[i][1]}")
-                print(f"Nama Gadget : {arr_ra[i][2]}")
-                print(f"Tanggal Peminjaman : {arr_ra[i][3]}")
-                print(f"Jumlah : {arr_ra[i][4]} \n")
-                if i-1 == 0 :
-                    flag1 = False
-                    flag2 = False
-                    break
-                else:
-                    i-=1
-            while flag2 :
-                tambahan = input("Tampilkan 5 data berikutnya? (Y/N) : ")
-                print()
-                if tambahan.lower() == 'n':
-                    flag1 = False
-                    flag2 = False
-                elif tambahan.lower()=='y':
-                    flag2 = False
-                else:
-                    print("Masukan Salah! Silahkan Ulangi")
-            if flag1 == False and flag2==False:
-                break
-
-def riwayatkembali():
-    filename = "gadget_return_history.csv"
-    arr_ra = csv_reader(filename)
-    i = len(arr_ra)-1
-    if len(arr_ra) == 1: #data kosong
-        print("Tidak ada data pengembalian gadget.")
-    else : # len(arr_ra) != 1
-        flag1 = True
-        while flag1 :
-            flag2 = True
-            for x in range(5): #descending per 5 data
-                print(f"ID Pengembalian : {arr_ra[i][0]}")
-                print(f"Nama Pengambil : {arr_ra[i][1]}")
-                print(f"Nama Gadget : {arr_ra[i][2]}")
-                print(f"Tanggal Pengembalian : {arr_ra[i][3]} \n")
-                if i-1 == 0 :
-                    flag1 = False
-                    flag2 = False
-                    break
-                else:
-                    i-=1
-            while flag2 :
-                tambahan = input("Tampilkan 5 data berikutnya? (Y/N) : ")
-                print()
-                if tambahan.lower() == 'n':
-                    flag1 = False
-                    flag2 = False
-                elif tambahan.lower()=='y':
-                    flag2 = False
-                else:
-                    print("Masukan Salah! Silahkan Ulangi")
-            if flag1 == False and flag2==False:
-                break
-
-def riwayatambil():
-    filename = "consumable_history.csv"
-    arr_ra = csv_reader(filename)
-    i = len(arr_ra)-1
-    if len(arr_ra) == 1: #data kosong
-        print("Tidak ada data pengambilan consumables.")
-    else : # len(arr_ra) != 1
-        flag1 = True
-        while flag1 :
-            flag2 = True
-            for x in range(5): #descending per 5 data
-                print(f"ID Pengambilan : {arr_ra[i][0]}")
-                print(f"Nama Pengambil : {arr_ra[i][1]}")
-                print(f"Nama Consumable : {arr_ra[i][2]}")
-                print(f"Tanggal Pengambilan : {arr_ra[i][3]}")
-                print(f"Jumlah : {arr_ra[i][4]} \n")
-                if i-1 == 0 :
-                    flag1 = False
-                    flag2 = False
-                    break
-                else:
-                    i-=1
-            while flag2 :
-                tambahan = input("Tampilkan 5 data berikutnya? (Y/N) : ")
-                print()
-                if tambahan.lower() == 'n':
-                    flag1 = False
-                    flag2 = False
-                elif tambahan.lower()=='y':
-                    flag2 = False
-                else:
-                    print("Masukan Salah! Silahkan Ulangi")
-            if flag1 == False and flag2==False:
-                break
-
-
-def help(x) :
-    if x == "user" :
-        print(" ================== HELP ==================")
-        print(" login - untuk melakukan login ke dalam sistem")
-        print(" carirarity - untuk mencari gadget dengan rarity tertentu")
-        print(" caritahun - untuk mencari gadget berdasarkan tahun ditemukan")
-        print(" pinjam - untuk melakukan peminjaman gadget")
-        print(" kembalikan - untuk mengembalikan gadget secara seutuhnya")
-        print(" minta - untuk meminta consumable yang tersedia")
-        print(" savedata - untuk melakukan penyimpanan ke dalam file")
-        print(" help - untuk memberikan panduan penggunaan sistem")
-    elif x == "admin" :
-        print(" ================== HELP ==================")
-        print(" register - untuk melakukan registrasi user baru")
-        print(" carararity - untuk mencari gadget dengan rarity tertentu")
-        print(" caritahun - untuk mencari gadget berdasarkan tahun ditemukan")
-        print(" tambahitem - untuk melakukan penambahan item")
-        print(" hapusitem - untuk menghapus suatu item pada database")
-        print(" ubahjumlah - untuk mengubah jumlah gadget dan consumable yang terdapat didalam sistem")
-        print(" riwayatpinjam - untuk melihat riwayat peminjaman gadget")
-        print(" riwayatkembali - untuk melihat riwayat pengembalian gadget")
-        print(" riwayatambil - untuk melihat riwayat pengambilan consumable")
-        print(" savedata - untuk melakukan penyimpanan ke dalam file")
-        print(" help - untuk memberikan panduan penggunaan sistem")
-
 def hapusitem():
     id = input("Masukkan ID item: ")
     if id[0].lower() == 'g':
@@ -314,13 +172,181 @@ def op_hapusitem(id, ref_arr):
             break
         return delete_by_id(id, ref_arr)
 
+def login():
+    while True:
+        a = input("Username: ")
+        p = input("Password: ")
+        if a == "farrel" and p == "rel111":
+            print("Logged in !\n")
+            break
+        else:
+            print("Wrong Username/Password")
 
+def riwayat(arr, mode):
+    if mode == "p": #pinjam (gadget)
+        arr_rp = arr
+        i = len(arr_rp) - 1
+        if len(arr_rp) == 1:  # data kosong
+            print("Tidak ada data peminjaman gadget.")
+        else:  # len(arr_rp) != 1
+            flag1 = True
+            while flag1:
+                flag2 = True
+                for x in range(5):  # descending per 5 data
+                    print(f"ID Peminjaman : {arr_rp[i][0]}")
+                    print(f"Nama Pengambil : {arr_rp[i][1]}")
+                    print(f"Nama Gadget : {arr_rp[i][2]}")
+                    print(f"Tanggal Peminjaman : {arr_rp[i][3]}")
+                    print(f"Jumlah : {arr_rp[i][4]} \n")
+                    if i - 1 == 0:
+                        flag1 = False
+                        flag2 = False
+                        break
+                    else:
+                        i -= 1
+                while flag2:
+                    tambahan = input("Tampilkan 5 data berikutnya? (Y/N) : ")
+                    print()
+                    if tambahan.lower() == 'n':
+                        flag1 = False
+                        flag2 = False
+                    elif tambahan.lower() == 'y':
+                        flag2 = False
+                    else:
+                        print("Masukan Salah! Silahkan Ulangi")
+                if flag1 == False and flag2 == False:
+                    break
+    elif mode == "k": #kembali (gadget)
+        arr_rk = arr
+        i = len(arr_rk) - 1
+        if len(arr_rk) == 1:  # data kosong
+            print("Tidak ada data pengembalian gadget.")
+        else:  # len(arr_rk) != 1
+            flag1 = True
+            while flag1:
+                flag2 = True
+                for x in range(5):  # descending per 5 data
+                    print(f"ID Pengembalian : {arr_rk[i][0]}")
+                    print(f"Nama Pengambil : {arr_rk[i][1]}")
+                    print(f"Nama Gadget : {arr_rk[i][2]}")
+                    print(f"Tanggal Pengembalian : {arr_rk[i][3]} \n")
+                    if i - 1 == 0:
+                        flag1 = False
+                        flag2 = False
+                        break
+                    else:
+                        i -= 1
+                while flag2:
+                    tambahan = input("Tampilkan 5 data berikutnya? (Y/N) : ")
+                    print()
+                    if tambahan.lower() == 'n':
+                        flag1 = False
+                        flag2 = False
+                    elif tambahan.lower() == 'y':
+                        flag2 = False
+                    else:
+                        print("Masukan Salah! Silahkan Ulangi")
+                if flag1 == False and flag2 == False:
+                    break
+    elif mode == "a" : #ambil (consumable)
+        arr_ra = arr
+        i = len(arr_ra) - 1
+        if len(arr_ra) == 1:  # data kosong
+            print("Tidak ada data pengambilan consumables.")
+        else:  # len(arr_ra) != 1
+            flag1 = True
+            while flag1:
+                flag2 = True
+                for x in range(5):  # descending per 5 data
+                    print(f"ID Pengambilan : {arr_ra[i][0]}")
+                    print(f"Nama Pengambil : {arr_ra[i][1]}")
+                    print(f"Nama Consumable : {arr_ra[i][2]}")
+                    print(f"Tanggal Pengambilan : {arr_ra[i][3]}")
+                    print(f"Jumlah : {arr_ra[i][4]} \n")
+                    if i - 1 == 0:
+                        flag1 = False
+                        flag2 = False
+                        break
+                    else:
+                        i -= 1
+                while flag2:
+                    tambahan = input("Tampilkan 5 data berikutnya? (Y/N) : ")
+                    print()
+                    if tambahan.lower() == 'n':
+                        flag1 = False
+                        flag2 = False
+                    elif tambahan.lower() == 'y':
+                        flag2 = False
+                    else:
+                        print("Masukan Salah! Silahkan Ulangi")
+                if flag1 == False and flag2 == False:
+                    break
 
-#main program
-ag_def = csv_reader('gadget.csv') #inisialisasi array gadget sementara
+def minta(arr):
+    arr_m = arr
+    found = False
+    while not(found):
+        id = input("Masukkan ID item: ")
+        for i in range(1, len(arr_m)):
+            if id == arr_m[i][0]:
+                found = True
+                r_idx = i
+        if found :
+            break
+        else:
+            print("ID item tidak tersedia. Silakan Ulangi!")
+    while True:
+        jml = int(input("Jumlah: "))
+        if jml <= int(arr_m[r_idx][3]):
+            break
+        else:
+            print(f"Permintaan tidak dapat dipenuhi")
+            print(f"Jumlah item tersisa : {arr_m[r_idx][3]}")\
+    #tanggal belum ada validasi
+    tgl = input("Tanggal permintaan: ")
+    print(f"Item {arr_m[r_idx][1]} (x{jml}) telah berhasil diambil!")
+    arr_m[r_idx][3] = str(int(arr_m[r_idx][3])-jml)
+    return arr_m
+
+def help(x):
+    if x == "user":
+        print(" ================== HELP ==================")
+        print(" login - untuk melakukan login ke dalam sistem")
+        print(" carirarity - untuk mencari gadget dengan rarity tertentu")
+        print(" caritahun - untuk mencari gadget berdasarkan tahun ditemukan")
+        print(" pinjam - untuk melakukan peminjaman gadget")
+        print(" kembalikan - untuk mengembalikan gadget secara seutuhnya")
+        print(" minta - untuk meminta consumable yang tersedia")
+        print(" savedata - untuk melakukan penyimpanan ke dalam file")
+        print(" help - untuk memberikan panduan penggunaan sistem")
+    elif x == "admin":
+        print(" ================== HELP ==================")
+        print(" register - untuk melakukan registrasi user baru")
+        print(" carararity - untuk mencari gadget dengan rarity tertentu")
+        print(" caritahun - untuk mencari gadget berdasarkan tahun ditemukan")
+        print(" tambahitem - untuk melakukan penambahan item")
+        print(" hapusitem - untuk menghapus suatu item pada database")
+        print(" ubahjumlah - untuk mengubah jumlah gadget dan consumable yang terdapat didalam sistem")
+        print(" riwayatpinjam - untuk melihat riwayat peminjaman gadget")
+        print(" riwayatkembali - untuk melihat riwayat pengembalian gadget")
+        print(" riwayatambil - untuk melihat riwayat pengambilan consumable")
+        print(" savedata - untuk melakukan penyimpanan ke dalam file")
+        print(" help - untuk memberikan panduan penggunaan sistem")
+
+# MAIN PROGRAM
+# def : default, cus : customized
+au_def = csv_reader("user.csv") #inisialisasi array user
+au_cus = csv_reader("user.csv")
+ag_def = csv_reader('gadget.csv')  # inisialisasi array gadget
 ag_cus = csv_reader('gadget.csv')
-ac_def = csv_reader('consumable.csv') #inisialisasi array consumable sementara
+ac_def = csv_reader('consumable.csv')  # inisialisasi array consumable
 ac_cus = csv_reader('consumable.csv')
+ach_def = csv_reader("consumable_history.csv") # inisialisasi array consumable history
+ach_cus = csv_reader("consumable_history.csv")
+agbh_def = csv_reader("gadget_borrow_history.csv") # inisialisasi array gadget borrow history
+agbh_cus = csv_reader("gadget_borrow_history.csv")
+agrh_def = csv_reader("gadget_return_history.csv") # inisialisasi array gadget return history
+agrh_cus = csv_reader("gadget_return_history.csv")
 
 while True:
     a = input(">>> ")
@@ -346,13 +372,16 @@ while True:
         print(ag_cus)
     elif a == "cons":
         print(ac_cus)
-    elif a == "consumable":
-        print(cons)
+    elif a == "minta":
+        ac_cus = minta(ac_cus)
     elif a == "riwayatambil":
-        riwayatambil()
+        riwayat(ach_def, "a")
+    elif a == "riwayatpinjam":
+        riwayat(agbh_def, "p")
+    elif a == "riwayatkembali":
+        riwayat(agrh_def, "k")
     elif a == "help":
         x = "admin"  # fadlin nanti tolong bikin status logged_in nya sebagai admin atau user, ini hanya contoh
         help(x)
     elif a == 'exit':
         break
-
